008730488813e9971df703263fa8004b1aeb1e86
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-17 12:52:42 -0500

    "Added ulItem element to header and updated JavaScript to toggle its display on button click"

--- src/script/header.js
+++ src/script/header.js
@@ -1,13 +1,16 @@
 const button = document.getElementById('burger');
 const header = document.getElementById('header');
+const ulItem = document.getElementById('ulItem')
 
 button.addEventListener('click', activate);
 
 function activate() {
 	if (button.classList.toggle('active') === true) {
 		header.style.display = 'flex';
+		ulItem.style.display = 'flex';
         header.style.animation='fade-in .5s forwards'
 	} else {
+		ulItem.style.display = 'none';
         header.style.animation='fade-out .5s forwards'
 	}
 }
